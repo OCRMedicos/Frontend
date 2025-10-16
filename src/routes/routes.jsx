f1a092f587b33631cadd2e3247993dfcc3623100
Maycacon <mayconbarroso2@gmail.com>
2025-10-16 16:36:42 -0300

    feat: add admin dashboard and user management functionality

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,6 +2,7 @@
 import { Routes, Route } from "react-router-dom"
 import Dashboard from "../pages/dashboard"
 import Login from "../pages/Longin"
+import DashboardAdmin from "../pages/dashboardAdmin"
 
 function RoutesNav() {
 
@@ -11,6 +12,7 @@ function RoutesNav() {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/admin" element={<DashboardAdmin />} />
             </Routes>
 
         </>
