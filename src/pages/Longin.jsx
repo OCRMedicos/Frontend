78a85e8d97bc5ad13c0802b6c3af0fdf0ba7430b
Ingrid Oliveira <132861259+iingridliveira@users.noreply.github.com>
2025-10-28 23:37:41 +0000

    doscs:components

--- src/pages/Longin.jsx
+++ src/pages/Longin.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleLeft, faEnvelope, faUnlockAlt } from "@fortawesome/free-solid-svg-icons";
-import { faFacebookF, faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { Col, Row, Form, Card, Button, FormCheck, Container, InputGroup } from '@themesberg/react-bootstrap';
-import { Link } from 'react-router-dom';
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faEnvelope, faUnlockAlt } from "@fortawesome/free-solid-svg-icons";
+import { Col, Row, Form, Card, FormCheck, Container, InputGroup } from '@themesberg/react-bootstrap';
+import { Link } from 'react-router-dom';
 import BgImage from "../assets/Img/signin.svg";
 
 
